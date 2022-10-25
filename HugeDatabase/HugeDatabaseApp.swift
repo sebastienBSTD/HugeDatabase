03d2ecf582cbc08c8822b8d74595c96cf086e9ff
rosqoe <rosqoe@icloud.com>
2022-10-25 16:10:21 +0200

    Initial Commit

---
+++ HugeDatabase/HugeDatabaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  HugeDatabaseApp.swift
+//  HugeDatabase
+//
+//  Created by Sebastien Bastide on 25/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct HugeDatabaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
